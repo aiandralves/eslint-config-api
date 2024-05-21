9d0b1fbd07c1d8581271238b02b2404da246ac4c
aiandralves <dinha.aiandra@yahoo.com.br>
2024-05-21 15:52:50 -0300

    Adicionando novas funcionalidades

---
+++ angular.js
@@ -0,0 +1,57 @@
+module.exports = {
+    root: true,
+    ignorePatterns: ["projects/**/*"],
+    plugins: ["unused-imports"],
+    overrides: [
+        {
+            files: ["*.ts"],
+            extends: [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@angular-eslint/recommended",
+                "plugin:@angular-eslint/template/process-inline-templates",
+            ],
+            rules: {
+                "no-unused-vars": "off",
+                "@typescript-eslint/no-unused-vars": "off",
+                "unused-imports/no-unused-imports": "error",
+                "unused-imports/no-unused-vars": [
+                    "warn",
+                    {
+                        vars: "all",
+                        varsIgnorePattern: "^_",
+                        args: "after-used",
+                        argsIgnorePattern: "^_",
+                    },
+                ],
+                "@angular-eslint/directive-selector": [
+                    "error",
+                    {
+                        type: "attribute",
+                        style: "camelCase",
+                    },
+                ],
+                "@angular-eslint/component-selector": [
+                    "error",
+                    {
+                        type: "element",
+                        style: "kebab-case",
+                    },
+                ],
+                semi: "off",
+                quotes: ["error", "double"],
+                indent: ["error", 4],
+                "max-len": ["error", 120],
+                "no-useless-constructor": "off",
+                "no-void": "off",
+                "no-unused-vars": "off",
+                "@typescript-eslint/no-explicit-any": "off",
+            },
+        },
+        {
+            files: ["*.html"],
+            extends: ["plugin:@angular-eslint/template/recommended", "plugin:@angular-eslint/template/accessibility"],
+            rules: {},
+        },
+    ],
+};
